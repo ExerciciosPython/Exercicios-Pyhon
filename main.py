93a31fbf2f57f9735df5d9c67398ae28ca52d763
ExercíciosPython <123480973+ExerciciosPython@users.noreply.github.com>
2023-05-25 10:12:46 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+class Calculadora:
+    """
+    classe Calculadora que faz as operações matemáticas:
+    - somar()
+    - subtrair()
+    - multiplicar()
+    - dividir()
+    - potenciar()
+    - radiciar()
+    - fatoriar()
+    - somatoria()
+    """
+
+    def somar(self, *numeros: int | float) -> 
